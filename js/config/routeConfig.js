5e50aea0ed295b8c17b0f8156e1c81e0e52465cc
Camilo Fernandes <camilo.analista@gmail.com>
2016-07-12 03:35:22 -0300

    finalizado rotas antigas

--- js/config/routeConfig.js
+++ js/config/routeConfig.js
@@ -2,12 +2,32 @@ angular.module("listaTelefonica").config(function($routeProvider){
 
   $routeProvider.when("/contatos", {
     templateUrl: "view/contatos.html",
-    controller: "listaTelefonicaCtrl"
+    controller: "listaTelefonicaCtrl",
+    resolve: {
+      contatos: function(contatosAPI){
+        return contatosAPI.getContatos();
+      },
+      operadoras: function(operadorasAPI){
+        return operadorasAPI.getOperadoras();
+      }
+    }
   });
 
   $routeProvider.when("/novoContato", {
     templateUrl: "view/novoContato.html",
-    controller: "listaTelefonicaCtrl"
+    controller: "novoContatoCtrl",
+    resolve: {
+      operadoras: function(operadorasAPI){
+        return operadorasAPI.getOperadoras();
+      }
+    }
   });
+
+  $routeProvider.when("/detalhesContato", {
+    templateUrl: "view/detalhesContato.html",
+    controller: "detalhesContatoCtrl"
+  });
+
+  $routeProvider.otherwise({redirectTo: "/contatos"});
 
 });
