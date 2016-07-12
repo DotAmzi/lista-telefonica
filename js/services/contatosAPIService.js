1c2eb326989c055593596d28c85e8ab812a22c8c
Camilo Fernandes <camilo.analista@gmail.com>
2016-07-12 04:21:01 -0300

    falta atualizar backend

--- js/services/contatosAPIService.js
+++ js/services/contatosAPIService.js
@@ -4,9 +4,9 @@ angular.module("listaTelefonica").factory("contatosAPI", function ($http, config
 	};
 
 	var _getContato = function (id) {
-		return $http.get(config.baseUrl + "/contatos" + id);
+		return $http.get(config.baseUrl + "/contatos/" + id);
 	};
-
+	
 	var _saveContato = function (contato) {
 		return $http.post(config.baseUrl + "/contatos", contato);
 	};
