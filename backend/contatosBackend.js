a280ca4bb21fbd9a2ab5f5d502d96acb80800eb2
Camilo <camilo.analista@gmail.com>
2016-07-14 17:42:28 -0300

    alterado backend

--- backend/contatosBackend.js
+++ backend/contatosBackend.js
@@ -1,26 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.bodyParser());
+app.use(bodyParser.json());
+
+var operadoras = [
+  {nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
+  {nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1},
+  {nome: "Tim", codigo: 41, categoria: "Celular", preco: 3},
+  {nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1},
+  {nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2}
+];
 
 var contatos = [
-	{id: 1, nome: "Bruno", telefone: "9999-2222", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}},
-	{id: 2, nome: "Sandra", telefone: "9999-3333", data: new Date(), operadora: {nome: "Vivo", codigo: 15, categoria: "Celular"}},
-	{id: 3, nome: "Mariana", telefone: "9999-9999", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}}
+  {id: 1, nome: "Bruno", telefone: "9999-2222", data: new Date(), operadora: operadoras[0]},
+  {id: 2, nome: "Sandra", telefone: "9999-3333", data: new Date(), operadora: operadoras[1]},
+  {id: 3, nome: "Mariana", telefone: "9999-9999", data: new Date(), operadora: operadoras[2]}
 ];
-var operadoras = [
-	{nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
-	{nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1},
-	{nome: "Tim", codigo: 41, categoria: "Celular", preco: 3},
-	{nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1},
-	{nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2}
-];
-
-
-app.listen(3412, function(){
-  console.log('Server no ar');
-})
 
 
 app.all('*', function(req, res, next) {
@@ -34,11 +30,25 @@ app.get('/contatos', function(req, res) {
   res.json(contatos);
 });
 
+app.get('/contatos/:id', function(req, res) {
+  contatos.forEach(function (contato) {
+    if (contato.id == req.params.id) {
+      res.json(contato);
+      return;
+    }
+  });
+  res.status(404).end();
+});
+
 app.post('/contatos', function(req, res) {
+  console.log(req.body);
   contatos.push(req.body);
+
   res.json(true);
 });
 
 app.get('/operadoras', function(req, res) {
   res.json(operadoras);
 });
+
+app.listen(process.env.PORT || 3412);
